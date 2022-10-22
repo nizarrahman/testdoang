63aee30af68e7cf39ff52d17ca082cedb03c532e
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-10-22 19:20:01 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,6 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0813-8694-5391]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
